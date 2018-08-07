652d8471cf2f5a9cb147e02ee9ae66641225db63
DevoRoberts <devorob26@gmail.com>
2018-08-06 20:01:40 -0400

    Added mongoose models

--- server/server.js
+++ server/server.js
@@ -5,17 +5,22 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true
 });
 
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
+// let Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
 
 // let newTodo = new Todo({
 //     text: 'Make a sandwich'
@@ -27,13 +32,49 @@ let Todo = mongoose.model('Todo', {
 //     console.log('Unable to save todo');
 // });
 
-let beGreat = new Todo({
-    text: 'Learn MongoDB and Mongoose',
-    completed: true
+// let beGreat = new Todo({
+//     text: 'Learn MongoDB and Mongoose',
+//     completed: true
+// });
+
+// beGreat.save().then((res) => {
+//     console.log('Be Great');
+// }, (e) => {
+//     console.log("Error being great, try again");
+// });
+
+// let beGreat = new Todo({
+
+// });
+
+// beGreat.save().then((res) => {
+//     console.log('Be Great');
+// }, (e) => {
+//     console.log("Error being great, try again");
+// });
+
+let User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
 });
 
-beGreat.save().then((res) => {
-    console.log('Be Great');
+let newUser = new User({
+    name: 'Devodriq',
+    email: 'test1@gmail.com'
+});
+
+newUser.save().then((result) => {
+    console.log('User Saved', result);
 }, (e) => {
-    console.log("Error being great, try again");
+    console.log('There was an error saving the user', e);
 });
